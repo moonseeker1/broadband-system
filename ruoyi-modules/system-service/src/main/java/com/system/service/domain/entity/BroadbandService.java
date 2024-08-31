83241a8e3b255c1887e0f3ce31f1bf6abbf43b2b
zeming pan <2290166829@qq.com>
2024-08-31 23:36:12 +0800

    代码自动生成

--- ruoyi-modules/system-service/src/main/java/com/system/service/domain/entity/BroadbandService.java
+++ ruoyi-modules/system-service/src/main/java/com/system/service/domain/entity/BroadbandService.java
@@ -1,5 +1,6 @@
 package com.system.service.domain.entity;
 
+import com.baomidou.mybatisplus.annotation.TableField;
 import com.baomidou.mybatisplus.annotation.TableName;
 import com.baomidou.mybatisplus.annotation.IdType;
 import java.time.LocalDateTime;
