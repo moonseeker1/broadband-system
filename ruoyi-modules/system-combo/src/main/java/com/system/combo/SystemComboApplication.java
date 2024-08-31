75a75124ca0b7b741b4ce95c1d0c3e1ac0ecfeeb
zeming pan <2290166829@qq.com>
2024-08-31 23:51:06 +0800

    服务初始化

---
+++ ruoyi-modules/system-combo/src/main/java/com/system/combo/SystemComboApplication.java
@@ -0,0 +1,36 @@
+package com.system.combo;
+
+import com.ruoyi.common.security.annotation.EnableCustomConfig;
+import com.ruoyi.common.security.annotation.EnableRyFeignClients;
+import com.ruoyi.common.swagger.annotation.EnableCustomSwagger2;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 系统模块
+ * 
+ * @author ruoyi
+ */
+@EnableCustomConfig
+@EnableCustomSwagger2
+@EnableRyFeignClients
+@SpringBootApplication
+@MapperScan("com.system.combo.mapper")
+public class SystemComboApplication
+{
+    public static void main(String[] args)
+    {
+        SpringApplication.run(SystemComboApplication.class, args);
+        System.out.println("(♥◠‿◠)ﾉﾞ  宽带套餐模块启动成功   ლ(´ڡ`ლ)ﾞ  \n" +
+                " .-------.       ____     __        \n" +
+                " |  _ _   \\      \\   \\   /  /    \n" +
+                " | ( ' )  |       \\  _. /  '       \n" +
+                " |(_ o _) /        _( )_ .'         \n" +
+                " | (_,_).' __  ___(_ o _)'          \n" +
+                " |  |\\ \\  |  ||   |(_,_)'         \n" +
+                " |  | \\ `'   /|   `-'  /           \n" +
+                " |  |  \\    /  \\      /           \n" +
+                " ''-'   `'-'    `-..-'              ");
+    }
+}
---
+++ ruoyi-modules/system-workorder/src/main/java/com/system/workorder/SystemWorkerorderApplication.java
@@ -0,0 +1,36 @@
+package com.system.workorder;
+
+import com.ruoyi.common.security.annotation.EnableCustomConfig;
+import com.ruoyi.common.security.annotation.EnableRyFeignClients;
+import com.ruoyi.common.swagger.annotation.EnableCustomSwagger2;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 系统模块
+ * 
+ * @author ruoyi
+ */
+@EnableCustomConfig
+@EnableCustomSwagger2
+@EnableRyFeignClients
+@SpringBootApplication
+@MapperScan("com.system.workorder.mapper")
+public class SystemWorkerorderApplication
+{
+    public static void main(String[] args)
+    {
+        SpringApplication.run(SystemWorkerorderApplication.class, args);
+        System.out.println("(♥◠‿◠)ﾉﾞ  工单模块启动成功   ლ(´ڡ`ლ)ﾞ  \n" +
+                " .-------.       ____     __        \n" +
+                " |  _ _   \\      \\   \\   /  /    \n" +
+                " | ( ' )  |       \\  _. /  '       \n" +
+                " |(_ o _) /        _( )_ .'         \n" +
+                " | (_,_).' __  ___(_ o _)'          \n" +
+                " |  |\\ \\  |  ||   |(_,_)'         \n" +
+                " |  | \\ `'   /|   `-'  /           \n" +
+                " |  |  \\    /  \\      /           \n" +
+                " ''-'   `'-'    `-..-'              ");
+    }
+}
