6e19b6aa9715e203dda65761a85816171259f64f
zeming pan <2290166829@qq.com>
2024-09-04 14:21:21 +0800

    定时扫描账户套餐情况

--- ruoyi-api/ruoyi-api-system/src/main/java/com/ruoyi/system/api/RemoteAccountService.java
+++ ruoyi-api/ruoyi-api-system/src/main/java/com/ruoyi/system/api/RemoteAccountService.java
@@ -2,6 +2,7 @@ package com.ruoyi.system.api;
 
 import com.ruoyi.common.core.constant.SecurityConstants;
 import com.ruoyi.common.core.domain.R;
+import com.ruoyi.common.core.web.domain.AjaxResult;
 import com.ruoyi.system.api.model.BroadbandAccount;
 import com.ruoyi.system.api.model.BusinessPeople;
 import org.springframework.cloud.openfeign.FeignClient;
@@ -12,7 +13,9 @@ import java.util.List;
 @FeignClient(contextId = "remoteAccountService", value = "system-account")
 public interface RemoteAccountService {
     @GetMapping("account/remote/list")
-    public R<List<BusinessPeople>> remoteList(BroadbandAccount broadbandAccount, @RequestHeader(SecurityConstants.FROM_SOURCE) String source);
+    public R<List<BroadbandAccount>> remoteList(BroadbandAccount broadbandAccount, @RequestHeader(SecurityConstants.FROM_SOURCE) String source);
     @GetMapping("/account/{id}")
     public R<BroadbandAccount> getById(@PathVariable("id") String id,@RequestHeader(SecurityConstants.FROM_SOURCE) String source);
+    @PostMapping("/account/update")
+    public AjaxResult update(@RequestBody BroadbandAccount broadbandAccount,@RequestHeader(SecurityConstants.FROM_SOURCE) String source);
 }
--- ruoyi-modules/ruoyi-job/src/main/java/com/ruoyi/job/task/RyTask.java
+++ ruoyi-modules/ruoyi-job/src/main/java/com/ruoyi/job/task/RyTask.java
@@ -1,7 +1,16 @@
 package com.ruoyi.job.task;
 
+import cn.hutool.core.date.DateTime;
+import com.ruoyi.common.core.constant.SecurityConstants;
+import com.ruoyi.system.api.RemoteAccountService;
+import com.ruoyi.system.api.RemoteComboService;
+import com.ruoyi.system.api.model.BroadbandAccount;
+import com.ruoyi.system.api.model.BroadbandCombo;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import com.ruoyi.common.core.utils.StringUtils;
+
+import java.util.List;
 
 /**
  * 定时任务调度测试
@@ -10,7 +19,10 @@ import com.ruoyi.common.core.utils.StringUtils;
  */
 @Component("ryTask")
 public class RyTask
-{
+{   @Autowired
+    RemoteAccountService remoteAccountService;
+    @Autowired
+    RemoteComboService remoteComboService;
     public void ryMultipleParams(String s, Boolean b, Long l, Double d, Integer i)
     {
         System.out.println(StringUtils.format("执行多参方法： 字符串类型{}，布尔类型{}，长整型{}，浮点型{}，整形{}", s, b, l, d, i));
@@ -24,5 +36,31 @@ public class RyTask
     public void ryNoParams()
     {
         System.out.println("执行无参方法");
+    }
+    public void accountComboScan(){
+        BroadbandAccount broadbandAccount = new BroadbandAccount();
+        List<BroadbandAccount> list = remoteAccountService.remoteList(broadbandAccount, SecurityConstants.INNER).getData();
+        for(int i=0;i<list.size();i++){
+            BroadbandCombo broadbandCombo = remoteComboService.get(broadbandAccount.getComboId(),SecurityConstants.INNER).getData();
+            if(broadbandAccount.getStatus().equals("1")){
+                if(broadbandAccount.getEndTime().before(DateTime.now())){
+                    if(broadbandAccount.getAmount().compareTo(broadbandCombo.getPrice())<0){
+                        broadbandAccount.setStatus("2");
+                        remoteAccountService.update(broadbandAccount,SecurityConstants.INNER);
+                    }
+                    else {
+                        broadbandAccount.setAmount(broadbandAccount.getAmount().subtract(broadbandCombo.getPrice()));
+                        remoteAccountService.update(broadbandAccount,SecurityConstants.INNER);
+                    }
+                }
+            }
+            if(broadbandAccount.getStatus().equals("3")){
+                if(broadbandAccount.getEndTime().before(DateTime.now())){
+                    broadbandAccount.setStatus("0");
+                    broadbandAccount.setComboId(null);
+                    remoteAccountService.update(broadbandAccount,SecurityConstants.INNER);
+                }
+            }
+        }
     }
 }
